595fb8b0ca9c4d9cf0d7fa15c901d8fb2500ab7b
skosty <skosty@utexas.edu>
2015-09-02 15:53:08 -0500

    Update SecuritySubject.java

--- SecuritySubject.java
+++ SecuritySubject.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-//package security1;
-
-/**
- *
- * 
- */
 public class SecuritySubject {
   
   String name = null;
